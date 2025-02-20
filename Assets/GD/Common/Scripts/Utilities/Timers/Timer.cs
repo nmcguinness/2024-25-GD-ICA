78b05a5060c6b443be99d647609765e7069c140b
Niall McGuinness <niall.mcguinness@dkit.ie>
2025-02-20 13:39:02 +0000

    Adding timer

--- Assets/GD/Common/Scripts/Utilities/Timers/Timer.cs
+++ Assets/GD/Common/Scripts/Utilities/Timers/Timer.cs
@@ -1,7 +1,28 @@
-﻿namespace GD.Utility
+﻿using UnityEngine;
+
+namespace GD.Utility
 {
+    /// <summary>
+    /// Support stopwatch and countdown timer functionality.
+    /// </summary>
     public class Timer
     {
-        //TODO: Implement Timer
+        private bool isRunning = false;
+        private float startTime = 0;
+
+        private float elapsedUpdateTime = 0;
+        private float elapsedFixedTime = 0;
+
+        public virtual void Start(int time = 0) => Reset(time);
+
+        public virtual void Reset(int time) => elapsedUpdateTime = elapsedFixedTime = time;
+
+        public virtual void Stop()
+        {
+        }
+
+        public virtual void Update() => elapsedUpdateTime += Time.deltaTime;
+
+        public virtual void FixedUpdate() => elapsedFixedTime += Time.fixedDeltaTime;
     }
 }
